9eb2b59c3e35c89ae1be2cd0750393d474e14fd7
Mads Jans <mads213@gmail.com>
2024-10-03 15:32:28 +0200

    Read csv files into array with numpy

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-if __name__ == 'main':
-    print('Hello world!')
+import numpy as np
+
+DIRPATH = './src/data/'
+STREAM_NAME = 'example_streams.csv'
+TOPOLOGY_NAME = 'example_topology.csv'
+
+stream_csv = np.loadtxt(DIRPATH + STREAM_NAME, delimiter=',', dtype=str)
+topology_csv = np.loadtxt(DIRPATH + TOPOLOGY_NAME, delimiter=',', dtype=str)
+
+print(stream_csv)
+    
+    
